fc04ec43800588a39e519015615a33b965ede194
SafeHeaded <m.patryk@outlook.com>
2022-03-28 17:38:13 +0200

    Chakra setup

---
+++ src/pages/_document.tsx
@@ -0,0 +1,17 @@
+import { ColorModeScript } from "@chakra-ui/react";
+import { NextPage } from "next"
+import { Html, Head, Main, NextScript } from "next/document";
+import { theme } from "../theme/theme";
+
+const Document: NextPage = () => {
+    return <Html lang="en">
+        <Head />
+        <body>
+            <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+            <Main />
+            <NextScript />
+        </body>
+    </Html>
+}
+
+export default Document;
---
+++ src/theme/theme.ts
@@ -0,0 +1,8 @@
+import { extendTheme, type ThemeConfig, Theme } from "@chakra-ui/react";
+
+const config: ThemeConfig = {
+  useSystemColorMode: false,
+  initialColorMode: "light",
+};
+
+export const theme = extendTheme({ config }) as Theme;
