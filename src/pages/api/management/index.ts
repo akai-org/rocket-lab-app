d404777f4aa5ccefb2634d0762ac2e0e9596dc41
SafeHeaded <m.patryk@outlook.com>
2022-04-09 12:38:04 +0200

    users api endpoint

---
+++ src/pages/api/management/index.ts
@@ -0,0 +1,24 @@
+import { withApiAuthRequired } from '@auth0/nextjs-auth0'
+import { User, userModel } from '../../../../mongo/models/user'
+
+export default withApiAuthRequired(async function items(req, res) {
+  if (req.method === 'PATCH') {
+    const users = JSON.parse(req.body) as User[]
+    const roles = new Set()
+    for (const user of users) {
+      roles.add(user.role)
+    }
+    const arrayRoles = Array.from(roles)
+    try {
+      for (const role of arrayRoles) {
+        const roleUsers = users.filter((user) => user.role === role)
+        const usersIds = roleUsers.map(({ _id }) => _id)
+        userModel.updateMany({ _id: { $in: usersIds } }, { role })
+      }
+      res.status(200).send({ message: 'Ussers Updated successfully' })
+    } catch (error) {
+      console.log(error)
+      res.status(500).send({ error })
+    }
+  }
+})
