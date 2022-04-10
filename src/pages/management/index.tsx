cbc661b4531e0775b18100f0b2583e782d2836c8
SafeHeaded <m.patryk@outlook.com>
2022-04-10 16:17:47 +0200

    frontend permissions added

--- src/pages/management/index.tsx
+++ src/pages/management/index.tsx
@@ -1,4 +1,5 @@
 import {
+  getSession,
   withPageAuthRequired,
   WithPageAuthRequiredOptions,
 } from '@auth0/nextjs-auth0'
@@ -36,10 +37,22 @@ const ManagementHome: NextPage<Props> = ({ users: propsUsers, error }) => {
 
 export default ManagementHome
 
+const neededRole = 'admin'
+
 export const getServerSideProps = withPageAuthRequired({
-  async getServerSideProps() {
+  async getServerSideProps({ req, res }) {
     try {
       await connectDB()
+
+      const session = getSession(req, res)
+
+      if (!session) throw new Error('Unauthorized access')
+
+      const user = await userModel.findById(
+        (session.user.sub as string).split('|')[1]
+      )
+
+      if (user?.role !== neededRole) throw new Error('Unauthorized access')
 
       const users = await userModel.find({}).limit(15)
 
@@ -51,8 +64,9 @@ export const getServerSideProps = withPageAuthRequired({
     } catch (error) {
       console.log(error)
       const errorMessage = error as Error
+      // TODO: To some redirecting with popup why redirected
       return {
-        props: { error: { message: errorMessage } },
+        props: { error: JSON.parse(JSON.stringify(errorMessage)) },
       }
     }
   },
