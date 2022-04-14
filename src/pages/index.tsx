b33cd314158a4a679432560edf4d3abb7fe4ef6c
whpac <marcinszwarc@hotmail.com>
2022-04-14 11:26:10 +0200

    withPageAuthRequired is now run on the server side

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,38 +12,28 @@ const Home: NextPage<Props> = ({ items }) => {
   return <ItemsList items={items ?? []} />
 }
 
-export default withPageAuthRequired(Home)
+export default Home
 
-//export const getServerSideProps = withPageAuthRequired()
+export const getServerSideProps = withPageAuthRequired({
+  getServerSideProps: async (): Promise<{props: Props}> => {
+    // Fetch the items we already have in the DB
+    // For now it's not wrapped into any specific function
+    await connectDB();
+    const items = await ItemModel.find();
 
-export const getServerSideProps = async (): Promise<{props: Props}> => {
-  // Fetch the items we already have in the DB
-  // For now it's not wrapped into any specific function
-  await connectDB();
-  const items = await ItemModel.find();
+    const itemData = items.map((item) => {
+      return {
+        id: item.id,
+        name: item.name,
+        description: item.description,
+        imageUrl: item.imageUrl
+      }
+    })
 
-  /*
-    {
-    _id: new ObjectId("624d825afe0a220d0c35e94e"),
-    name: 'test',
-    imageUrl: 'testtttt',
-    description: 'test',
-    toBuy: false,
-    __v: 0
-  }
-  */
-  const itemData = items.map((item) => {
     return {
-      id: item.id,
-      name: item.name,
-      description: item.description,
-      imageUrl: item.imageUrl
+      props: {
+        items: itemData,
+      },
     }
-  })
-
-  return {
-    props: {
-      items: itemData,
-    },
   }
-}
+})
