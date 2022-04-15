170b8e59e1ef3ef9b6b12b36d1f6314822109c47
whpac <marcinszwarc@hotmail.com>
2022-04-15 12:36:57 +0200

    Added error catching

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,17 @@ import { withPageAuthRequired } from '@auth0/nextjs-auth0'
 import ItemsList, { ItemProps } from '../components/Storage/ItemsList/ItemsList'
 import { connectDB } from '../mongo/db';
 import { ItemModel } from '../mongo/models/item';
+import { Text } from '@chakra-ui/react'
 
 interface Props {
   items?: ItemProps[]
+  error?: Error
 }
 
-const Home: NextPage<Props> = ({ items }) => {
-  return <ItemsList items={items ?? []} />
+const Home: NextPage<Props> = ({ items, error }) => {
+  return (!error ?
+    <ItemsList items={items ?? []} /> :
+    <Text>{error.message}</Text>)
 }
 
 export default Home
@@ -18,22 +22,31 @@ export const getServerSideProps = withPageAuthRequired({
   getServerSideProps: async (): Promise<{props: Props}> => {
     // Fetch the items we already have in the DB
     // For now it's not wrapped into any specific function
-    await connectDB();
-    const items = await ItemModel.find();
+    try{
+      await connectDB();
+      const items = await ItemModel.find();
 
-    const itemData = items.map((item) => {
+      const itemData = items.map((item) => {
+        return {
+          id: item.id,
+          name: item.name,
+          description: item.description,
+          imageUrl: item.imageUrl
+        }
+      })
+
       return {
-        id: item.id,
-        name: item.name,
-        description: item.description,
-        imageUrl: item.imageUrl
+        props: {
+          items: itemData,
+        },
       }
-    })
-
-    return {
-      props: {
-        items: itemData,
-      },
+    }catch(e){
+      console.log(e)
+      return {
+        props: {
+          error: JSON.parse(JSON.stringify(e))
+        },
+      }
     }
   }
 })
