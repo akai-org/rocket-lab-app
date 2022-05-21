028f5078a192895792d3ec386364850d8777cce7
Patryk Marczak <m.patryk@outlook.com>
2022-05-21 19:50:18 +0200

    adding new cart list

--- src/services/cartService.ts
+++ src/services/cartService.ts
@@ -2,10 +2,18 @@ import { CartItemModel } from '../mongo/models/cart'
 import { Item } from '../mongo/models/item'
 
 export interface FutureCartItem {
-    id: string
-    quantity: number
+  item: string
+  quantity: number
 }
 
-export async function createNewCart(cartName: string, cartItems: string[]) {
-    const newCartItem = await CartItemModel.create({name: cartName, })
+export async function createNewCart(
+  cartName: string,
+  cartItems: FutureCartItem[]
+) {
+  const newCartItem = await CartItemModel.create({
+    name: cartName,
+    items: cartItems,
+  })
+
+  return newCartItem
 }
