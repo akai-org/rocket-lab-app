97218a06e02f5e1d74b90754b65eaccc259bcafc
Kacper Nowaczyk <kacpern30@gmail.com>
2022-05-29 19:55:02 +0200

    add acordion component

--- src/components/List/DesktopList/List/List.tsx
+++ src/components/List/DesktopList/List/List.tsx
@@ -8,19 +8,36 @@ import {
   Thead,
   Tr,
   useDisclosure,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
 } from '@chakra-ui/react'
 import ProductButton from '../../../UI/Custom Buttons/ProductButton/ProductButton'
 import ModalEditList from '../../../UI/Modals/ModalEditList/ModalEditList'
-import {
-  CartItem,
-  CartList,
-  PopulatedCartList,
-} from '../../../../mongo/models/cart'
-import { Item } from '../../../../mongo/models/item'
+import { PopulatedCartList } from '../../../../mongo/models/cart'
 import DeletePopover from '../../../UI/Popovers/DeletePopover'
 import ListItem from '../ListItem/ListItem'
 
 export interface Props extends PopulatedCartList {}
+
+;<Accordion allowMultiple>
+  <AccordionItem>
+    <AccordionButton>
+      <Box flex="1" textAlign="left">
+        Section 1 title
+      </Box>
+      <AccordionIcon />
+    </AccordionButton>
+    <AccordionPanel pb={4}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.
+    </AccordionPanel>
+  </AccordionItem>
+</Accordion>
 
 const List = (props: Props) => {
   const {
@@ -29,26 +46,33 @@ const List = (props: Props) => {
     onClose: onCloseEditList,
   } = useDisclosure()
   return (
-    <Box
+    <Accordion
+      allowMultiple
       borderRadius="6px"
       bgColor="white"
       border="1px solid #C4C4C4"
       mt="20px"
     >
-      <Flex flexWrap="wrap" p="20px">
-        {/* TODO: Usuwanie listy */}
-        <Flex w="100%" justifyContent="space-between">
-          <Heading
-            fontSize="20px"
-            lineHeight="10px"
-            mt="15px"
-            color="#4A5568"
-            fontWeight="600"
-            mb="15px"
-            ml="20px"
-          >
-            {props.name}
-          </Heading>
+      <AccordionItem>
+        <Flex>
+          <AccordionButton>
+            <Box flex="1" textAlign="left">
+              <Flex w="100%" justifyContent="space-between">
+                <Heading
+                  fontSize="20px"
+                  lineHeight="10px"
+                  mt="15px"
+                  color="#4A5568"
+                  fontWeight="600"
+                  mb="15px"
+                  ml="20px"
+                >
+                  {props.name}
+                </Heading>
+              </Flex>
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
           <Flex pt="5px" mr="20px">
             <ProductButton
               size="sm"
@@ -64,23 +88,28 @@ const List = (props: Props) => {
             />
           </Flex>
         </Flex>
-        <Table p="20px">
-          <Thead>
-            <Tr fontSize="16px" fontWeight="700">
-              <Th minW="250px">NAZWA</Th>
-              <Th w="100%">OPIS</Th>
-              <Th textAlign="right" minW="170px">
-                ILOŚĆ SZTUK
-              </Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {props.items.map((item) => (
-              <ListItem key={item.id} {...item} />
-            ))}
-          </Tbody>
-        </Table>
-      </Flex>
+        <AccordionPanel pb={4}>
+          <Flex flexWrap="wrap" p="20px">
+            {/* TODO: Usuwanie listy */}
+            <Table p="20px">
+              <Thead>
+                <Tr fontSize="16px" fontWeight="700">
+                  <Th minW="250px">NAZWA</Th>
+                  <Th w="100%">OPIS</Th>
+                  <Th textAlign="right" minW="170px">
+                    ILOŚĆ SZTUK
+                  </Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {props.items.map((item) => (
+                  <ListItem key={item.id} {...item} />
+                ))}
+              </Tbody>
+            </Table>
+          </Flex>
+        </AccordionPanel>
+      </AccordionItem>
       <ModalEditList
         list={props.items}
         name="defaultowa lista"
@@ -88,7 +117,7 @@ const List = (props: Props) => {
         isOpen={isOpenEditList}
         isCentered
       />
-    </Box>
+    </Accordion>
   )
 }
 
