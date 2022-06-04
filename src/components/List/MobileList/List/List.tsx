104449c1492b6c5abc93296ad3b911049ebd49bd
Patryk Marczak <m.patryk@outlook.com>
2022-06-04 17:15:54 +0200

    fixed cartList deletion on mobile

--- src/components/List/MobileList/List/List.tsx
+++ src/components/List/MobileList/List/List.tsx
@@ -18,10 +18,29 @@ import ListItem from '../ListItem/ListItem'
 import ModalEditList from '../../../UI/Modals/ModalEditList/ModalEditList'
 import { PopulatedCartList } from '../../../../mongo/models/cart'
 import ListMenu from '../../../UI/Menus/ListMenu'
+import { useDispatch } from 'react-redux'
+import { removeExisitngCartList } from '../../../../store/Slices/storageCartSlice'
+import { fetcher } from '../../../../utils/requests'
 
 export interface Props extends PopulatedCartList {}
 
 const List = (props: Props) => {
+  const dispatch = useDispatch()
+
+  const deleteCartList = async () => {
+    try {
+      const deletedCartList = await fetcher(
+        'http://localhost:3000/api/cart/delete',
+        {
+          method: 'DELETE',
+          body: { id: props.id },
+        }
+      )
+      dispatch(removeExisitngCartList(deletedCartList))
+    } catch (error) {
+      console.log(error)
+    }
+  }
   const {
     isOpen: isOpenEditList,
     onOpen: onOpenEditList,
@@ -52,7 +71,7 @@ const List = (props: Props) => {
             <AccordionIcon />
           </AccordionButton>
           <Flex pt="5px" px="10px" w="5%" justifyContent="flex-end">
-            <ListMenu onEdit={onOpenEditList} onDelete={() => {}} />
+            <ListMenu onEdit={onOpenEditList} onDelete={deleteCartList} />
           </Flex>
         </Flex>
         <AccordionPanel pb={4}>
