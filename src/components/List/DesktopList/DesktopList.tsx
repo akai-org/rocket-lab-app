882d225057224a7cf59144dce54fa06c481a43b9
Patryk Marczak <m.patryk@outlook.com>
2022-06-05 16:45:42 +0200

    hid addItem in /list

--- src/components/List/DesktopList/DesktopList.tsx
+++ src/components/List/DesktopList/DesktopList.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { CartListsProps } from '../../../pages/list'
 import { storageCartInfo } from '../../../store/store'
+import { IS_DEV } from '../../../utils/constants'
 import { fetcher } from '../../../utils/requests'
 import AddItem from './AddItem/AddItem'
 import List from './List/List'
@@ -15,7 +16,7 @@ const DesktopList = () => {
   return (
     <Flex flexDirection="row" w="100vw" maxW="2000px" m="75px auto 0 auto">
       <Flex w="100%" flexDirection="column" ml="223px" p="40px">
-        <AddItem />
+        {IS_DEV && <AddItem />}
         {storageCartData.cartLists.map((cartList) => (
           <List {...cartList} key={cartList.id} />
         ))}
--- src/components/List/MobileList/MobileList.tsx
+++ src/components/List/MobileList/MobileList.tsx
@@ -1,6 +1,7 @@
 import { Flex } from '@chakra-ui/react'
 import { useSelector } from 'react-redux'
 import { storageCartInfo } from '../../../store/store'
+import { IS_DEV } from '../../../utils/constants'
 import AddItem from './AddItem/AddItem'
 import List from './List/List'
 
@@ -8,7 +9,7 @@ const MobileList = () => {
   const storageCartData = useSelector(storageCartInfo)
   return (
     <Flex mt="80px" color="#3F3F3F" flexDirection="column">
-      <AddItem />
+      {IS_DEV && <AddItem />}
       {storageCartData.cartLists.map((cartList) => (
         <List {...cartList} key={cartList.id} />
       ))}
