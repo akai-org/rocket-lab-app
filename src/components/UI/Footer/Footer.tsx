22cf437f5e0bc9087a38269c65b3f64de7562329
Alan Grądecki <alangradecki@wp.pl>
2022-07-10 01:00:56 +0200

    implemented footer

---
+++ src/components/UI/Footer/Footer.tsx
@@ -0,0 +1,20 @@
+import { Flex, Text, Image } from '@chakra-ui/react'
+
+const Footer = () => {
+  return (
+    <Flex
+      bg="#FFFFFF"
+      w="100vw"
+      h="3rem"
+      borderTop="1px solid #D5D5D5"
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Image src="logo-akai.png" alt="Logo AKAI" />
+      <Text>Powered by AKAI</Text>
+    </Flex>
+  )
+}
+
+export default Footer
