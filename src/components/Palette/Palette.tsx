d830635eaca8d470625bfe976651c023911393fc
Kacper Nowaczyk <kacpern30@gmail.com>
2022-07-12 18:54:40 +0200

    created helper to palette

--- src/components/Palette/Palette.tsx
+++ src/components/Palette/Palette.tsx
@@ -1,8 +1,29 @@
-import { HStack, Text, useColorMode, useMediaQuery } from '@chakra-ui/react'
+import {
+  BoxProps,
+  HStack,
+  Text,
+  useColorMode,
+  Box,
+  useMediaQuery,
+} from '@chakra-ui/react'
 import DesktopWrapper from '../UI/Wrappers/DesktopWrapper/DesktopWrapper'
 import MobileWrapper from '../UI/Wrappers/MobileWrapper/MobileWrapper'
 import { useColors } from '../../theme/useColors'
 import ColorModeSwitch from '../UI/ColorModeSwitch/ColorModeSwitch'
+
+const Rect = (rectProps: BoxProps) => (
+  <Box
+    w="100%"
+    h="4rem"
+    fontSize="20px"
+    border="2px solid black"
+    mt="5px"
+    borderRadius="10px"
+    {...rectProps}
+  >
+    {rectProps.children}
+  </Box>
+)
 
 const Palette = () => {
   const [isDesktop] = useMediaQuery('(min-width: 900px)')
@@ -18,8 +39,11 @@ const Palette = () => {
       </HStack>
       <Text color={colors.primary}>Primary</Text>
       <Text color={colors.secondary}>Secondary</Text>
-      <Text color={colors.borderPrimary}>BorderPrimary</Text>
-      <Text color={colors.borderSecondary}>BorderSecondary</Text>
+      <Text color={colors.neutral}>Neutral</Text>
+      <Rect bgColor={colors.background}>Background</Rect>
+      <Rect bgColor={colors.borderPrimary}>BorderPrimary</Rect>
+      <Rect bgColor={colors.borderSecondary}>BorderSecondary</Rect>
+      <Rect bgColor={colors.shadow}>Shadow</Rect>
     </Wrapper>
   )
 }
--- src/theme/useColors.ts
+++ src/theme/useColors.ts
@@ -4,23 +4,24 @@ export const useColors = () => {
   const { colorMode } = useColorMode()
   const mode = colorMode === 'light'
 
+  //First value for light mode
+  //Second value for dark mode
   const primary = mode ? 'black' : 'white'
   const secondary = mode ? '#3F3F3F' : '#C0C0C0'
-  const disabled = mode ? '#C4C4C4' : 'red'
-  const shadow = mode ? '' : ''
+  const neutral = mode ? '#2D3748' : '#D5D7CA'
+  const shadow = mode ? '#D5D5D5' : 'black'
+  const background = mode ? 'white' : '#1A202C'
 
-  const menu = '#878585'
   const borderPrimary = '#C4C4C4'
   const borderSecondary = '#D4D4D4'
 
-  const colors = {
+  return {
     primary,
     secondary,
+    neutral,
+    shadow,
+    background,
     borderPrimary,
     borderSecondary,
-    disabled,
-    shadow,
   }
-
-  return colors
 }
