8b9096eabb81e12df04f1756efb3372bb92618cb
Alan Grądecki <alangradecki@wp.pl>
2022-07-17 23:59:47 +0200

    added wrapper in DesktopHistory

--- src/ui/modules/History/DesktopHistory/DesktopHistory.tsx
+++ src/ui/modules/History/DesktopHistory/DesktopHistory.tsx
@@ -2,23 +2,16 @@ import { Flex, Text, Box } from '@chakra-ui/react'
 import Filter from './Filter/Filter'
 import HistoryList from './HistoryList/HistoryList'
 import { useColors } from '../../../../theme/useColors'
+import DesktopWrapper from '../../../components/Wrappers/DesktopWrapper/DesktopWrapper'
 
 const DesktopHistory = () => {
   const colors = useColors()
 
   return (
-    <Flex
-      flexDirection="row"
-      w="100vw"
-      maxW="2000px"
-      m="75px auto 0 auto"
-      minW="900px"
-      color={colors.fontSecondary}
-    >
+    <DesktopWrapper>
       <Flex
         w="100%"
         flexDirection="column"
-        m="30px 40px 20px 250px"
         p="40px"
         border={`1px solid ${colors.borderSecondary}`}
         borderRadius="6px"
@@ -33,7 +26,7 @@ const DesktopHistory = () => {
           <HistoryList />
         </Flex>
       </Flex>
-    </Flex>
+    </DesktopWrapper>
   )
 }
 
