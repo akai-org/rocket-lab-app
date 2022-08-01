c1414cdf26eaf5a2b54860ca7959d28bb72b7915
Patryk Marczak <m.patryk@outlook.com>
2022-08-01 18:33:15 +0200

    items connected to logs

--- src/pages/api/items/delete.ts
+++ src/pages/api/items/delete.ts
@@ -1,5 +1,9 @@
 import { withApiAuthRequired } from '@auth0/nextjs-auth0'
 import { NextApiHandler } from 'next'
+import {
+  createHistoryLog,
+  validateSession,
+} from '../../../services/historyService'
 import * as itemsService from '../../../services/itemsService'
 import { withMiddleware } from '../../../utils/middlewares'
 
@@ -7,7 +11,14 @@ const handler: NextApiHandler = async (req, res) => {
   if (req.method === 'DELETE') {
     const body = req.body
     try {
+      const session = validateSession(req, res)
       const deletedItem = await itemsService.deleteItem(body.id)
+      if (!deletedItem) throw new Error('No deleted item found')
+      await createHistoryLog(session.user.email, 'removed', {
+        name: deletedItem.name,
+        type: 'item',
+        description: deletedItem.description,
+      })
       res.status(200).send(deletedItem)
     } catch (error) {
       console.log(error)
--- src/pages/api/items/update.ts
+++ src/pages/api/items/update.ts
@@ -1,15 +1,25 @@
 import { withApiAuthRequired } from '@auth0/nextjs-auth0'
 import { NextApiHandler } from 'next'
+import {
+  createHistoryLog,
+  validateSession,
+} from '../../../services/historyService'
 import * as itemsService from '../../../services/itemsService'
 import { withMiddleware } from '../../../utils/middlewares'
 
 const handler: NextApiHandler = async (req, res) => {
   if (req.method === 'PUT') {
     const body = req.body
-    console.log(body)
     try {
-      const items = await itemsService.updateItem(body.id, body.item)
-      res.status(200).send(items)
+      const session = validateSession(req, res)
+      const updatedItem = await itemsService.updateItem(body.id, body.item)
+      if (!updatedItem) throw new Error('No item updated')
+      await createHistoryLog(session.user.email, 'modified', {
+        name: updatedItem.name,
+        type: 'item',
+        description: updatedItem.description,
+      })
+      res.status(200).send(updatedItem)
     } catch (error) {
       console.log(error)
       res.status(400).send(error)
